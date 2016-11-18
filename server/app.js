41cf339501501826beb8f009827c451daa21eaac
emilykeenan <emilyjanekeenan@gmail.com>
2016-11-18 16:54:10 -0600

    initial commit

---
+++ server/app.js
@@ -0,0 +1,18 @@
+var express =  require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Catchall route
+app.get('/', function (req, res) {
+  res.sendFile(path.resolve('./server/public/views/index.html'));
+});
+
+app.use(express.static('./server/public'));
+
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), function () {
+  console.log('Listening on port ', app.get('port'));
+});
